938da5f111d238bfc5ef031ebf62e4a78abb6b0c
PREKNOWLEDGEWEB <83751707+PREKNOWLEDGEWEB@users.noreply.github.com>
2021-10-22 20:35:18 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,32 @@ export default function Home() {
         <link href="https://mdbootstrap.com/previews/mdb-ui-kit/sidenav/css/mdb.min.css" rel="stylesheet"/>
         <Header title="Welcome to PBrowse!" />
         <p className="description">
-          Join Discord for downloads and updates: <a href="discord.gg/3qS69y5UK2">Join!</a>
-          <img src="https://www.pbrowse.ml/webres/shot-1.png" class="card-img-top" alt="PBrowse "/>
+          Join Discord for downloads and updates: <a class="btn btn-info" href="discord.gg/3qS69y5UK2">Join!</a>
+              <div class="container">
+              <div class="row">
+
+              <div class="col-4 mb-2">
+              <div class="card" style="width: 18rem;">
+                <img src="https://www.pbrowse.ml/webres/shot-1.png" class="card-img-top" alt="PBrowse "/>
+                <div class="card-body">
+                  <h5 class="card-title">Quick Link</h5>
+                  <p class="card-text">Quickly Visit sites via quick links easy</p>
+                </div>
+              </div>
+              </div>
+
+              <div class="col-4 mb-2">
+              <div class="card" style="width: 18rem;">
+                <img src="https://www.pbrowse.ml/webres/shot-2.png" class="card-img-top" alt="PBrowse "/>
+                <div class="card-body">
+                  <h5 class="card-title">View News</h5>
+                  <p class="card-text">Latest news on your newtab page</p>
+                </div>
+              </div>
+              </div>
+
+              </div>
+              </div>
         </p>
       </main>
 
